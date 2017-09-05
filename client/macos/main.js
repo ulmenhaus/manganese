30d4c98c1a62cab903d2924245c5ad9de7f4e187
Ryan Abrams <rdabrams@gmail.com>
2017-09-05 14:48:28 -0700

    initial tests

---
+++ client/macos/main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron');
+const shell = require('electron').shell;
+const globalShortcut = require('electron').globalShortcut;
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false
+    },
+    width: 800,
+    height: 600,
+    frame: false}
+			 );
+
+  win.loadURL('https://docker.slack.com');
+
+  win.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+  return win;
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
