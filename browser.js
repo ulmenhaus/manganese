4998c8eeb6f80fc26c90b13ed49e1d764850562c
Ryan Abrams <rdabrams@gmail.com>
2017-12-31 12:58:00 -0800

    WIP apps def into dict

--- browser.js
+++ browser.js
@@ -1,8 +1,135 @@
 //window.onresize = doLayout;
 let $ = require('jquery');
 
+spaces = [
+  {
+    name: "Chats",
+    pages: [
+      {
+	name: "Docker",
+	icon: "https://icon-icons.com/icons2/478/PNG/512/slack_47017.png",
+	url: "https://docker.slack.com/",
+      },
+      {
+	name: "Ulmenhaus",
+	icon: "https://icon-icons.com/icons2/478/PNG/512/slack_47017.png",
+	url: "https://ulmenhaus.slack.com/",
+      },
+      {
+	name: "Telegram",
+	icon: "https://image.flaticon.com/icons/png/512/355/355977.png",
+	url: "https://web.telegram.org/",
+      },
+      {
+	name: "Whatsapp",
+	icon: "https://seeklogo.com/images/W/whatsapp-icon-logo-8CA4FB831E-seeklogo.com.png",
+	url: "https://web.whatsapp.com/",
+      },
+      {
+	name: "Personal",
+	icon: "https://image.flaticon.com/icons/svg/355/355992.svg",
+	url: "https://mail.google.com/mail/u/1/",
+      },
+      {
+	name: "Work",
+	icon: "https://image.flaticon.com/icons/svg/355/355992.svg",
+	url: "https://mail.google.com/mail/u/0/",
+      },
+      {
+	name: "Pushbullet",
+	icon: "http://icons.iconarchive.com/icons/blackvariant/button-ui-requests-5/1024/PushBullet-icon.png",
+	url: "https://www.pushbullet.com/#sms",
+      },
+      {
+	name: "Twitter",
+	icon: "https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-twitter-512.png",
+	url: "https://www.twitter.com",
+      },
+    ],
+  },
+  {
+    name: "News",
+    pages: [
+      {
+	name: "Reddit",
+	icon: "https://vignette2.wikia.nocookie.net/siivagunner/images/0/07/Reddit_icon.svg/revision/latest?cb=20160623172208",
+	url: "https://www.reddit.com/",
+      },
+      {
+	name: "Hackernews",
+	icon: "http://www.pvhc.net/img114/tjxujdxphndbchnacbgw.png",
+	url: "https://news.ycombinator.com/",
+      },
+      {
+	name: "LessWrong",
+	icon: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/LessWrong_logo.svg/1280px-LessWrong_logo.svg.png",
+	url: "http://lesswrong.com/",
+      },
+      {
+	name: "Euronews",
+	icon: "http://www.userlogos.org/files/logos/Hugo/euronews1.png",
+	url: "http://www.euronews.com/",
+      },
+      {
+	name: "TechCrunch",
+	icon: "http://botaware.com/wp-content/uploads/2014/10/techcrunch-icon-1.png",
+	url: "https://techcrunch.com/",
+      },
+      {
+	name: "Lifehacker",
+	icon: "https://pbs.twimg.com/profile_images/590971614545612800/eV8h6rz7.png2D",
+	url: "https://lifehacker.com/",
+      },
+      {
+	name: "El Cultural",
+	icon: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwj7oPbx1KjYAhWnjlQKHeK3CfUQjBwIBA&url=http%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F662605729929568256%2F_9GXy5GK_400x400.png&psig=AOvVaw3KJs-ORCjCuImBcn2zTxdi&ust=1514411574786607",
+	url: "http://www.elcultural.com/",
+      },
+      {
+	name: "BBC",
+	icon: "http://icons.iconarchive.com/icons/martz90/circle/512/bbc-news-icon.png",
+	url: "http://www.bbc.com/",
+      },
+      {
+	name: "The Daily Beast",
+	icon: "https://lh5.ggpht.com/4bci6bTnSI8HVYH4bsh53jjHxb52_X6XiKK7niOaXmzoY3kiCLes4wvuK3SGurGd5Bg=w300",
+	url: "https://www.thedailybeast.com/",
+      },
+      {
+	name: "Brookings",
+	icon: "https://cdn-images-1.medium.com/fit/c/200/200/0*gR4ta5uBTkSqnxfe.png",
+	url: "https://www.brookings.edu/",
+      },
+    ],
+  },
+]
+
+// TODO refactor!
 onload = function() {
-  var currentSpace = 2;
+  // TODO react would be better
+  var body = ""
+  var topbar = '<div id="topbar" class="topbar"><div style="padding-top: 30px; padding-left: 20px; position: absolute;">Spaces</div>'
+
+  for (var i=0; i<spaces.length; i++) {
+    var index = i + 1;
+    var space = spaces[i]
+    topbar += '<div style="padding-top: 30px; padding-left:' +  (100 * index) + 'px; position: absolute;">' + index + ' ' + space.name + '</div>';
+    body += '<div class="space" id="space' + index + '"><div class="leftbar">'
+    body +=  '<div class="leftbarbutton">' + space.name + '<br /><br /></div>'
+    for (var j=0; j<space.pages.length; j++) {
+      var page = space.pages[j]
+      body += '<div class="leftbarbutton"><img class="leftbaricon" src="' + page.icon + '" /><br />' + index + ' ' + page.name + '</div>'
+    }
+    body += '</div><div class="chatcontainer">'
+    for (var j=space.pages.length - 1; j>=0; j--) {
+      var page = space.pages[j]
+      body += '<webview id="space' + index + '_view' + (j + 1) + '" class="pageview" src="' + page.url + '"></webview>'
+    }
+    body += '</div></div>'
+  }
+  topbar += '</div>'
+  $("body").html(body + topbar)
+  var currentSpace = spaces.length;
 
   document.addEventListener('keydown', (e) => {
     if (!e.getModifierState("Control") || isNaN(e.key))
@@ -65,6 +192,7 @@ onload = function() {
     $("#space" + e.key).show()
   });
 
+  // TODO move post-scripts to JSON
   var wv = document.querySelector("#space1_view6")
   wv.addEventListener('did-finish-load', () => {
     var contents = wv.getWebContents();
